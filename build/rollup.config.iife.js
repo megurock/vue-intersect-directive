b5d6516980c7a7ef43ed9e4d370f3cfc32f3f2c3
Eiji Meguro <eiji@fork.co.jp>
2019-08-20 20:58:30 +0900

    minify script for browser

--- build/rollup.config.iife.js
+++ build/rollup.config.iife.js
@@ -1,15 +1,17 @@
 import base from './rollup.config.base'
+import { terser } from 'rollup-plugin-terser'
 
 const config = Object.assign({}, base, {
   output: {
     format: 'iife',
     exports: 'named',
     name: 'VueIntersectDirective',
-    file: './dist/vue-intersect-directive.iife.js',
+    file: './dist/vue-intersect-directive.min.js',
     globals: {
       vue: 'Vue'
     },
   },
 })
+config.plugins.push(terser())
 
 export default config
